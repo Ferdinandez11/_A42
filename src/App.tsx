9b193765e8094cc92732e3f6c9fb553c5b22d312
Ferdinandez11 <ferdinandez11@gmail.com>
2025-12-01 23:28:09 +0100

    compilar_CRM

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter, Routes, Route, Link, Outlet, useNavigate } from 'react-router-dom';
 import { supabase } from './lib/supabase';
 
